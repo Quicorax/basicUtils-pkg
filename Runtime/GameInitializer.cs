34d2d1d5bfb90a79b8039231154f7b10073e88f1
Quicorax <qcolobrans@gmail.com>
2022-11-07 16:35:41 +0100

    Game Initializer

---
+++ Runtime/GameInitializer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Quicorax
+{
+    public class GameInitializer : MonoBehaviour
+    {
+        [SerializeField]
+        private CoroutinerService corroutinerPref;
+        private CoroutinerService corroutinerInstance;
+
+        private void Start()
+        {
+            corroutinerInstance = Instantiate(corroutinerPref);
+
+            LevelInitializer();
+        }
+
+        private void LevelInitializer()
+        {
+            ServiceLoader serviceLoader = new();
+            serviceLoader.LoadServices(corroutinerInstance);
+        }
+    }
+}
