11fde940afce2a672fe91e9365e5b3f8abac4f74
Quicorax <qcolobrans@gmail.com>
2022-11-08 17:32:29 +0100

    Addressables

--- Runtime/AddressablesService.cs
+++ Runtime/AddressablesService.cs
@@ -14,6 +14,8 @@ namespace Quicorax
 
         public void LoadAdrsAsset(string adrsKey, Action<GameObject> onAssetLoaded) =>
             _coroutiner.RunCoroutine(AssetLoded(adrsKey, onAssetLoaded));
+        public void LoadAdrsOfComponent<T>(string key, Transform parent, Action<T> taskAction) =>
+            LoadAdrsOfComponentAsync(key, parent, taskAction);
 
         private IEnumerator AssetLoded(string adrsKey, Action<GameObject> onAssetLoaded)
         {
@@ -21,6 +23,18 @@ namespace Quicorax
             yield return ardsAsset;
 
             onAssetLoaded?.Invoke(ardsAsset.Result);
+        }
+
+        private IEnumerator LoadAdrsOfComponentAsync<T>(string key, Transform parent, Action<T> taskAction)
+        {
+            var ardsAsset = Addressables.LoadAssetAsync<GameObject>(key);
+            yield return ardsAsset;
+
+            var adrsObject = Addressables.InstantiateAsync(key, parent);
+            yield return adrsObject;
+
+            GameObject loadedAsset = adrsObject.Result;
+            taskAction?.Invoke(loadedAsset.GetComponent<T>());
         }
     }
 }
